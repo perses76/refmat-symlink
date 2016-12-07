e99acb0095cd86e0a42672b0573fb241f2864a57
Vadim Ovcearenco <wadim@veles-soft.com>
2016-12-07 09:03:02 +0200

    add --db argument to main.py

--- refmat_symlink/main.py
+++ refmat_symlink/main.py
@@ -3,6 +3,7 @@ from ref_mat import RefMat
 from config import Config
 import argparse
 parser = argparse.ArgumentParser()
+
 parser.add_argument(
     "tags",
     type=str,
@@ -17,6 +18,13 @@ parser.add_argument(
     nargs='*'
 )
 
+parser.add_argument(
+    "--db",
+    type=str,
+    help="Path to refmatdb. Default: ~/refmatdb",
+    default=os.path.join(os.path.expanduser('~user'), 'refmatdb'),
+)
+
 
 SOLUTION_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 REFMAT_SYMLINK_DB = os.path.join(SOLUTION_DIR, 'db')
@@ -24,7 +32,7 @@ REFMAT_SYMLINK_DB = os.path.join(SOLUTION_DIR, 'db')
 
 def main():
     args = parser.parse_args()
-    ref_mat = RefMat(config=Config(root=REFMAT_SYMLINK_DB))
+    ref_mat = RefMat(config=Config(root=args.db))
 
     if args.files is None:
         files_for_import = ref_mat.get_all_inbox_items()
